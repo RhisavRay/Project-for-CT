abd842cc8ecc45a6196a9802c4d13ed553db3e29
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2024-04-19 01:46:33 +0530

    Basic skeleton of navbar is built

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import "../styles/navbar.css"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+
+        <div className="navitem">Image</div>
+
+        <div className="navitem links">
+
+            <div className="link">01. History</div>
+
+            <div className="link">02. Team</div>
+
+        </div>
+
+    </div>
   )
 }
 
