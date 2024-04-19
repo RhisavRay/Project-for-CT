1d0bcee40389fc9378071faac114e811af90dd36
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2024-04-19 12:42:54 +0530

    done

--- src/components/Three.jsx
+++ src/components/Three.jsx
@@ -2,7 +2,27 @@ import '../styles/three.css'
 
 const Three = () => {
   return (
-    <div>Three</div>
+    <div className="container">
+
+        <img src={require('../Assets/Image_3.png')} alt="Losangeles Mountains" className="image"/>
+
+        <div className="three_box">
+
+            <div className="number">02</div>
+
+            <div className="title">CLIMB</div>
+
+            <div className="text">
+
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum iste ipsa ratione quibusdam consequuntur rerum vitae praesentium eum pariatur. Nihil a tenetur praesentium similique earum vitae dolorem, iure delectus distinctio
+                </p>
+
+            </div>
+
+        </div>
+
+    </div>
   )
 }
 
