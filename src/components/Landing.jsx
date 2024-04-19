f89b4908f04869bc1b0266f066d70f35895f2849
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2024-04-19 11:11:29 +0530

    Call One in Landing

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
 import "../styles/landing.css"
+import One from './One'
 
 const Landing = () => {
 
   const [curImgIndex, setCurImgIndex] = useState(0)
 
   return (
-    <div>Landing</div>
+    <div>
+      <One/>
+    </div>
   )
 }
 
